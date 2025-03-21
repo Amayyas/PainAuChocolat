1a32fb6c4533249611d02ee2705c711a8c396e7a
Amayyas <amayyas.aouadene@epitech.eu>
2025-03-21 15:10:17 +0100

    [UPGRADE] add project

--- main.c
+++ main.c
@@ -7,20 +7,49 @@
 
 #include "my.h"
 
-int main(int argc, char **argv)
+void print_usage(char *program_name)
 {
-    int nb1 = 0;
-    int nb2 = 0;
+    printf("USAGE: %s number1 number2 [number3 ...]\n", program_name);
+}
+
+int add_numbers(int count, char **numbers)
+{
     int sum = 0;
 
-    if (argc != 3) {
-        printf("Error: please provide exactly two numbers.\n");
-        printf("USAGE: %s number1 number2\n", argv[0]);
+    for (int i = 0; i < count; i++) {
+        sum += atoi(numbers[i]);
+    }
+    return sum;
+}
+
+int error_handling(int argc, char **argv)
+{
+    if (argc == 2 && strcmp(argv[1], "-h") == 0) {
+        print_usage(argv[0]);
+        return 0;
+    }
+    if (argc < 3) {
+        printf("Not enough arguments.\n");
         return 84;
     }
-    nb1 = atoi(argv[1]);
-    nb2 = atoi(argv[2]);
-    sum = nb1 + nb2;
-    printf("The sum of %d and %d is %d\n", nb1, nb2, sum);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    int sum = add_numbers(argc - 1, &argv[1]);
+    int err = error_handling(argc, argv);
+
+    if (err != 0) {
+        return err;
+    }
+    printf("The sum of ");
+    for (int i = 1; i < argc; i++) {
+        printf("%s", argv[i]);
+        if (i < argc - 1) {
+            printf(" and ");
+        }
+    }
+    printf(" is %d\n", sum);
     return 0;
 }
