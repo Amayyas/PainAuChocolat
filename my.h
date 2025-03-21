05d2d4251fecbf9fab433793523b47613b470b4b
Amayyas <amayyas.aouadene@epitech.eu>
2025-03-21 15:10:37 +0100

    [UPGRADE] .header for add project

--- my.h
+++ my.h
@@ -10,7 +10,11 @@
 
     #include <stdio.h>
     #include <stdlib.h>
+    #include <string.h>
 
+void print_usage(char *program_name);
+int add_numbers(int count, char **numbers);
+int error_handling(int argc, char **argv);
 int main(int argc, char **argv);
 
 #endif
