c4c42553d307a16663e29b38968aa589a872d0ae
Amayyas <amayyas.aouadene@epitech.eu>
2025-03-21 14:50:47 +0100

    [ADD] .header for add project

---
+++ my.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** PainAuChocolat_Tester
+** File description:
+** my
+*/
+
+#ifndef INCLUDED_MY_H
+    #define INCLUDED_MY_H
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+int main(int argc, char **argv);
+
+#endif
